3bbe87d525bad2537b40bba1d446fdff5915d24d
Shin Yamaga <shinyamaga@ShinnoMacBook-Air.local>
2024-01-08 22:34:03 +0900

    create trigger

--- init/init.sql
+++ init/init.sql
@@ -30,3 +30,29 @@ GRANT readonly TO developer_read;
 
 CREATE USER developer_write WITH PASSWORD 'developer_write';
 GRANT readwrite TO developer_write;
+
+
+-- create tables
+create table item (
+  id integer, 
+  name varchar(10)
+);
+
+create table archived_item (
+  id integer, 
+  name varchar(10)
+);
+
+-- create trigger
+CREATE FUNCTION archive() RETURNS trigger AS $archive$  --引数なし、戻り値trigger型の関数として宣言 
+BEGIN
+  --トリガを呼び出すINSERT文の結果を使ってidと商、余りをanswerテーブルに挿入する
+  INSERT INTO archived_item VALUES (NEW.id, NEW.name);  
+  RETURN NEW;  --trigger型変数のNEWをRETURNする。
+END;
+$archive$
+LANGUAGE plpgsql;   --言語を指定
+
+
+CREATE TRIGGER archive AFTER INSERT ON item FOR EACH ROW
+EXECUTE FUNCTION archive();
